c1c762c1980d0831371881befc7134e6b73021b2
difuster <iam0kay@yandex.ru>
2021-12-06 19:28:38 +0600

    3rd step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,46 @@
-const { program } = require('commander');
+#!/usr/bin/env node
+
+import { program } from 'commander';
+import path from 'path';
+import process from 'process';
+import { readFileSync } from 'fs';
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
-  .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format');
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => {
+    const parseFile = (file) => {
+      return readFileSync(path.resolve('../fixtures', file));
+    }
+    const file1 = JSON.parse(parseFile(filepath1));
+    const file2 = JSON.parse(parseFile(filepath2));
+    const getKeys = (file) => {
+      return Object.keys(file);
+    }
+    const keys1 = getKeys(file1);
+    const keys2 = getKeys(file2);
+    let keys = keys1.concat(keys2).sort();
+    keys = keys.filter((item, index) => {
+      return keys.indexOf(item) === index;
+    });
+    console.log('{');
+    for (let i = 0; i < keys.length; i += 1) {
+      if (file1.hasOwnProperty(keys[i]) && file2.hasOwnProperty(keys[i])) {
+        if (file1[keys[i]] !== file2[keys[i]]) {
+          console.log(`  - ${keys[i]}: ${file1[keys[i]]}\n  + ${keys[i]}: ${file2[keys[i]]}`);
+        } else { 
+          console.log(`    ${keys[i]}: ${file1[keys[i]]}`);
+        }
+      } else if (file1.hasOwnProperty(keys[i]) && !file2.hasOwnProperty(keys[i])) {  
+        console.log(`  - ${keys[i]}: ${file1[keys[i]]}`);
+      } else if (!file1.hasOwnProperty(keys[i]) && file2.hasOwnProperty(keys[i])) {  
+        console.log(`  + ${keys[i]}: ${file2[keys[i]]}`);
+      }
+    }
+    console.log('}');
+  })
 
   program.parse();
