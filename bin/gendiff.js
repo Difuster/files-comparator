a7b9af1e9fba0516975218026af2113ab0e4c586
difuster <iam0kay@yandex.ru>
2021-12-11 18:00:26 +0600

    4 step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import path from 'path';
-import { readFileSync } from 'fs';
+import { genDiff } from '../gendifference.js';
 
 program
   .description('Compares two configuration files and shows a difference.')
@@ -11,29 +10,7 @@ program
   .argument('<filepath2>')
   .option('-f, --format [type]', 'output format')
   .action((filepath1, filepath2) => {
-    const parseFile = (file) => readFileSync(path.resolve('../fixtures', file));
-    const file1 = JSON.parse(parseFile(filepath1));
-    const file2 = JSON.parse(parseFile(filepath2));
-    const getKeys = (file) => Object.keys(file);
-    const keys1 = getKeys(file1);
-    const keys2 = getKeys(file2);
-    let keys = keys1.concat(keys2).sort();
-    keys = keys.filter((item, index) => keys.indexOf(item) === index);
-    console.log('{');
-    for (let i = 0; i < keys.length; i += 1) {
-      if (file1.hasOwnProperty(keys[i]) && file2.hasOwnProperty(keys[i])) {
-        if (file1[keys[i]] !== file2[keys[i]]) {
-          console.log(`  - ${keys[i]}: ${file1[keys[i]]}\n  + ${keys[i]}: ${file2[keys[i]]}`);
-        } else {
-          console.log(`    ${keys[i]}: ${file1[keys[i]]}`);
-        }
-      } else if (file1.hasOwnProperty(keys[i]) && !file2.hasOwnProperty(keys[i])) {
-        console.log(`  - ${keys[i]}: ${file1[keys[i]]}`);
-      } else if (!file1.hasOwnProperty(keys[i]) && file2.hasOwnProperty(keys[i])) {
-        console.log(`  + ${keys[i]}: ${file2[keys[i]]}`);
-      }
-    }
-    console.log('}');
+    console.log(genDiff(filepath1, filepath2));
   });
 
 program.parse();
---
+++ gendifference.js
@@ -0,0 +1,55 @@
+import path from 'path';
+import { readFileSync } from 'fs';
+import _ from 'lodash';
+
+const fileExtension = (file) => {
+  return path.extname(file);
+};
+
+const readFile = (file) => readFileSync(path.resolve('..', '__fixtures__', file), 'utf-8');
+
+const parseFile = (file, extension) => {
+  if (extension === '.json') {
+    const data = JSON.parse(file);
+    return data;
+  }
+};
+
+const compareData = (data1, data2) => {
+  const getKeys = (data) => Object.keys(data);
+  const keys1 = getKeys(data1);
+  const keys2 = getKeys(data2);
+  let keys = _.union(keys1, keys2).sort();
+  let result = '';
+  keys = keys.filter((item, index) => keys.indexOf(item) === index);
+  result += `{\n`;
+  for (let i = 0; i < keys.length; i += 1) {
+    if (_.has(data1, keys[i]) && _.has(data2, keys[i])) {
+      if (data1[keys[i]] !== data2[keys[i]]) {
+        result += `  - ${keys[i]}: ${data1[keys[i]]}\n  + ${keys[i]}: ${data2[keys[i]]}\n`;
+      } else if (data1[keys[i]] === data2[keys[i]]) {
+        result += `    ${keys[i]}: ${data1[keys[i]]}\n`;
+      }
+    }
+    if (_.has(data1, keys[i]) && !_.has(data2, keys[i])) {
+      result += `  - ${keys[i]}: ${data1[keys[i]]}\n`;
+    }
+    if (!_.has(data1, keys[i]) && _.has(data2, keys[i])) {
+      result += `  + ${keys[i]}: ${data2[keys[i]]}\n`;
+    }
+  }
+  result += `}`;
+  return result;
+};
+
+const genDiff = (file1, file2) => {
+  const fileExt1 = fileExtension(file1);
+  const fileExt2 = fileExtension(file2);
+  const readedFile1 = readFile(file1);
+  const readedFile2 = readFile(file2);
+  const data1 = parseFile(readedFile1, fileExt1);
+  const data2 = parseFile(readedFile2, fileExt2);
+  return compareData(data1, data2);
+};
+
+export { genDiff, compareData };
