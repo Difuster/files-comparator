d90067a725f5b15e5641d2a90ff27e06fcb44d89
difuster <iam0kay@yandex.ru>
2022-01-06 14:16:42 +0600

    modified parser.js

--- src/parser.js
+++ src/parser.js
@@ -4,16 +4,16 @@ import { readFileSync } from 'fs';
 
 const getPath = (file) => path.resolve('..', '__fixtures__', file);
 const getValue = (file) => readFileSync(file, 'utf-8');
-const getExtension = (file) => path.extname(file);
+const getExtension = (file) => path.extname(file).slice(1);
 
 const parseData = (file) => {
   const filePath = getPath(file);
   const fileValue = getValue(filePath);
   const fileExtension = getExtension(file);
-  if (fileExtension === '.json') {
+  if (fileExtension === 'json') {
     return JSON.parse(fileValue);
   }
-  if (fileExtension === '.yaml' || fileExtension === '.yml') {
+  if (fileExtension === 'yaml' || fileExtension === 'yml') {
     return yaml.load(fileValue);
   }
   return new Error('Unknown data format');
